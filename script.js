4c51d745a7e51a3c234d43589b221e108a0fdd48
sahibsiddiqui15@gmail.com <sahibsiddiqui15@gmail.com>
2025-04-19 20:01:42 +0530

    adding the quizapp on git

---
+++ script.js
@@ -0,0 +1,128 @@
+const questions = [
+    {
+        question: "What is the capital of France?",
+        answers: [
+            { text: "New York", correct: false },
+            { text: "London", correct: false },
+            { text: "Paris", correct: true },
+            { text: "Dublin", correct: false }
+        ]
+    },
+    {
+        question: "What is the capital of Japan?",
+        answers: [
+            { text: "Tokyo", correct: true },
+            { text: "Seoul", correct: false },
+            { text: "Beijing", correct: false },
+            { text: "Bangkok", correct: false }
+        ]
+    },
+    {
+        question: "What is the largest planet in our solar system?",
+        answers: [
+            { text: "Earth", correct: false },
+            { text: "Jupiter", correct: true },
+            { text: "Saturn", correct: false },
+            { text: "Mars", correct: false }
+        ]
+    },
+    {
+        question: "Who killed Amrendra Bahubali?",
+        answers: [
+            { text: "Raju Mama", correct: false },
+            { text: "Bhallaladeva", correct: false },
+            { text: "Katappa", correct: true },
+            { text: "Shivgami", correct: false }
+        ]
+    },
+    {
+        question: "What is the smallest ocean in the world?",
+        answers: [
+            { text: "Atlantic Ocean", correct: false },
+            { text: "Indian Ocean", correct: false },
+            { text: "Arctic Ocean", correct: true },
+            { text: "Pacific Ocean", correct: false }
+        ]
+    }
+]
+//WATCH MAXM JS THEN COPY
+const questionElement = document.getElementById('question');
+const answerButtons = document.getElementById("ans-btns");
+const nextButton = document.getElementById('next-btn');
+
+let currentQuestionIndex = 0;
+let score = 0;
+
+function startQuiz(){
+    currentQuestionIndex = 0;
+    score = 0;
+    nextButton.innerHTML = "Next";
+    showQuestion();
+}
+
+function showQuestion(){
+    resetState();
+    let currentQuestion = questions[currentQuestionIndex];
+    let questionNo = currentQuestionIndex + 1;
+    questionElement.innerHTML = questionNo + "] " + currentQuestion.question;
+
+    currentQuestion.answers.forEach(answer => {
+        const button = document.createElement("button");
+        button.innerHTML = answer.text;
+        button.classList.add("btn");
+        answerButtons.appendChild(button);
+        if(answer.correct){
+            button.dataset.correct = answer.correct;
+        }
+        button.addEventListener("click", selectAnswer);
+    })
+}
+
+function resetState(){
+    nextButton.style.display = "none";
+    while(answerButtons.firstChild){
+        answerButtons.removeChild(answerButtons.firstChild);
+    }
+}
+
+function selectAnswer(e){
+    const selectedBtn = e.target;
+    const isCorrect = selectedBtn.dataset.correct === "true";
+    if(isCorrect){
+        selectedBtn.classList.add("correct");
+        score++;
+    }else{
+        selectedBtn.classList.add("wrong");
+    }
+    Array.from(answerButtons.children).forEach(button => {
+        if(button.dataset.correct === "true"){
+            button.classList.add("correct");
+        }
+        button.disabled = true;
+    })
+    nextButton.style.display = "block"; 
+}
+
+function showScore(){
+    resetState();
+    questionElement.innerHTML = "Your Score: " + score + "/" + questions.length;
+    nextButton.innerHTML = "Quiz Again?";
+    nextButton.style.display = "block";
+}
+function handleNextButton(){
+    currentQuestionIndex++;
+    if(currentQuestionIndex < questions.length){
+        showQuestion();
+    }else{
+        showScore();
+    }
+}
+nextButton.addEventListener("click", () => {
+    if(currentQuestionIndex < questions.length){
+        handleNextButton();
+    }else{
+        startQuiz();
+    }
+})
+
+startQuiz();
